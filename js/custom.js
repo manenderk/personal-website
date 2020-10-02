4b17d6f8029cefd60fa357357c9e55990f05ff3d
Manender <manender369@gmail.com>
2020-10-02 05:26:26 -0400

    initial setup

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+
+/*Magnific Popup*/
+   $(function() {
+    $('div.gallery').magnificPopup({delegate: 'a', 
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      removalDelay: 300,
+      mainClass: 'mfp-fade'
+    });
+   });
+
+/*Animated Headline*/
+$(function() {
+  $('.selector').animatedHeadline();
+})
+/*Swiper*/
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 4000,
+  },
+});
+/*Preloader*/
+$(".preloader-wrap").delay(1500).fadeOut('slow');
